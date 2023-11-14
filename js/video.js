6af9ed35a0f5d68c501d19f0fe950b0bbc6095ed
hellenxt <143536540+hellenxt@users.noreply.github.com>
2023-11-14 00:05:18 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,7 +1,7 @@
 var video;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+	console.log("Now opening the window")
 	video = document.querySelector("#player1")
 	video.autoplay = false;
 	console.log("Autoplay is set to " + video.autoplay)
@@ -11,30 +11,30 @@ window.addEventListener("load", function() {
 
 
 document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video");
+	console.log("Play the Video");
 	video.play()
 	document.querySelector('#volume').innerHTML = video.volume * 100 + "%"
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	console.log("Pause Video");
+	console.log("Pause the Video");
 	video.pause()
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
-	console.log("Slow down");
+	console.log("Slow Down the Video");
 	video.playbackRate *= 0.9
 	console.log("Playback rate is " + video.playbackRate)
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	console.log("Speed up video");
+	console.log("Speed Up the Video");
 	video.playbackRate /= 0.9
 	console.log("Playback rate is " + video.playbackRate)
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
-	console.log("Skip ahead");
+	console.log("Skip Ahead");
 	video.currentTime += 10
 	if (video.currentTime >= video.duration)
 		video.currentTime = 0
@@ -44,13 +44,13 @@ document.querySelector("#skip").addEventListener("click", function() {
 document.querySelector("#mute").addEventListener("click", function(){
 	video.muted = !video.muted;
 	document.querySelector('#mute').innerHTML = video.muted ? 'Unmute' : 'Mute';
-	console.log('Is it Muted: ', video.muted)
+	console.log('Is the Video Muted: ', video.muted)
 })
 
 // Volume Slider
 document.querySelector("#slider").addEventListener
 ("change", function(){
-	console.log("I am in slider")
+	console.log("Now in Slider")
     console.log(this)
     console.log(this.value)
 	video.volume = document.querySelector("#slider").value / 100;
